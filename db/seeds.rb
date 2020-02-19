5fd9ba27897ab242d7dea7ecd53fe745b041586c
ICanDoAllThingThroughChrist <ctlee9096@gmail.com>
2020-02-19 09:17:41 -0600

    updating seed file as needed for demands

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,20 +19,6 @@
 # #  # binding.pry
 # # }
 #
-require 'csv'
-# for City of Houston Container Problems download
-columns = %w[SR_Number Client Service_Location Status Client_Str_No Client_Str_Name Client_Zip_Code
-  Phone_Number Email_Address Create_Date Due_Date Closed_Date	Overdue Agent_Name Super_Neighborhood
-  Tax_ID Service_Area District Key_Map Management_District GARBAGE_ROUTE GARBAGE_DAY GARBAGE_QUAD
-  RECYCLE_ROUTE RECYCLE_DAY RECYCLE_QUAD HEAVY_TRASH_DAY HEAVY_TRASH_QUAD Subject Reason service_type
-  Queue SLA Container_Problem Container_Damage Case_Note Resolution_Comment Channel_Type Other_Description
-  Title x y LATITUDE LONGITUDE Tax_ID1 unique]
-CSV.foreach("C:/Users/e128289/Documents/containers-problems.csv", {encoding: "iso-8859-1:utf-8", headers: true, header_converters: :symbol, converters: :all}) {|row|
-  # binding.pry
-  Cart.create(row.to_hash)
-  # binding.pry
-}
-#
 # require 'csv'
 # # for City of Houston BI Data Collection
 # # columns = %i[CASE_NUMBER	SR_LOCATION	COUNTY	CLIENT	STREET_NUM	CLIENT_STREET	CITY	STATE	ZIP	PHONE_NUMBER	EMAIL_ADDRESS	DISTRICT	NEIGHBORHOOD	TAX_ID	GARBAGE_ROUTE	GARBAGE_DAY1	GARBAGE_QUAD	RECYCLE_DAY1	RECYCLE_ROUTE	RECYCLE_QUAD	HEAVY_TRASH_DAY	HEAVY_TRASH_QUAD	KEY_MAP	MANAGEMENT_DISTRICT	SR_OWNER	SR_CREATOR	DEPARTMENT	DIVISION	SR_TYPE	QUEUE	SLA	STATUS	SR_CREATE_DATE	DUE_DATE	DATE_CLOSED	RESOLUTION_TIME	OVERDUE]
@@ -50,30 +36,39 @@ CSV.foreach("C:/Users/e128289/Documents/containers-problems.csv", {encoding: "is
 #  Assignment.create(row.to_hash)
 #   # binding.pry
 # }
-# require 'open-uri'
-# require 'csvreader'
-# require 'byebug'
+#
 # require 'csv'
-# web2 = open('https://hfdapp.houstontx.gov/311/311-Public-Data-Extract-2019-clean.txt'){|f| f.read}
-# web1 = open('https://hfdapp.houstontx.gov/311/311-Public-Data-Extract-monthly-clean.txt'){|f| f.read}
-# things1 = web1.split(/\n/)
-# things2 = web2.split(/\n/)
-#
-# columns = %i[case_number sr_location county district neighborhood tax_id trash_quad recycle_quad trash_day heavy_trash_day recycle_day key_map management_district department division sr_type queue sla status sr_create_date due_date date_closed overdue title x y latitude longitude channel_type created_at updated_at]
-#
-# things2.each {|sr|
-#     # byebug
-#     b=sr.split('|')
-#     c=Hash[columns.zip(b)]
-#     # byebug
-#     Sr.create(c)
-#     # byebug
-#  }
-#  things1.each {|sr|
-#      # byebug
-#      b=sr.split('|')
-#      c=Hash[columns.zip(b)]
-#      # byebug
-#      Sr.create(c)
-#      # byebug
-#   }
+# # for City of Houston Container Problems by Residents Service Request download
+# 1. Before Running Report: In CSV File, rename attribute from "type" to "service_type"
+# CSV.foreach("C:/Users/e128289/Documents/containers-problems.csv", {encoding: "iso-8859-1:utf-8", headers: true, header_converters: :symbol, converters: :all}) {|row|
+#   # binding.pry
+#   Cart.create(row.to_hash)
+#   # binding.pry
+# }
+require 'open-uri'
+require 'csvreader'
+require 'byebug'
+require 'csv'
+web2 = open('https://hfdapp.houstontx.gov/311/311-Public-Data-Extract-2019-clean.txt'){|f| f.read}
+web1 = open('https://hfdapp.houstontx.gov/311/311-Public-Data-Extract-monthly-clean.txt'){|f| f.read}
+things1 = web1.split(/\n/)
+things2 = web2.split(/\n/)
+
+columns = %i[case_number sr_location county district neighborhood tax_id trash_quad recycle_quad trash_day heavy_trash_day recycle_day key_map management_district department division sr_type queue sla status sr_create_date due_date date_closed overdue title x y latitude longitude channel_type created_at updated_at]
+
+things2.each {|sr|
+    # byebug
+    b=sr.split('|')
+    c=Hash[columns.zip(b)]
+    # byebug
+    Sr.create(c)
+    # byebug
+ }
+ things1.each {|sr|
+     # byebug
+     b=sr.split('|')
+     c=Hash[columns.zip(b)]
+     # byebug
+     Sr.create(c)
+     # byebug
+  }
