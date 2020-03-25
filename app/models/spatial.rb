89cb105b8817fc34b42e838510eac70b3b66ad60
ICanDoAllThingThroughChrist <ctlee9096@gmail.com>
2020-03-25 16:59:48 -0500

    map included in application!

--- app/models/spatial.rb
+++ app/models/spatial.rb
@@ -5,7 +5,8 @@ class Spatial < ApplicationRecord
     #import with Copy and Paste from NoQuadOverdue and NoQuadNotOverdue into below file
     #"C:\Users\e128289\Documents\NoQuadOverdue.txt"
     #"C:\Users\e128289\Documents\NoQuadNotOverdue.txt"
-    CSV.foreach("C:/Users/e128289/Documents/spatial-join-quad-assignments.csv", {encoding: "iso-8859-1:utf-8", headers: true, header_converters: :symbol, converters: :all}) {|row|
+    CSV.foreach("C:/Users/e128289/Documents/spatial-join-quad-assignments.csv",
+      {encoding: "iso-8859-1:utf-8", headers: true, header_converters: :symbol, converters: :all}) {|row|
       Spatial.create(row.to_hash)
     }
   end
