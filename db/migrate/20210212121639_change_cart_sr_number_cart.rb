3b809ec67ad88ff67a36cbf617c2a3575bf4adbe
lee <ctlee9096@gmail.com>
2021-06-16 13:31:11 -0500

    updated gemfile and cast integer for Postgres

--- db/migrate/20210212121639_change_cart_sr_number_cart.rb
+++ db/migrate/20210212121639_change_cart_sr_number_cart.rb
@@ -1,7 +1,7 @@
 class ChangeCartSrNumberCart < ActiveRecord::Migration[5.2]
   def change
-    change_column :carts, :cart_sr_number, :integer, limit: 8
-    change_column :carts, :cart_number, :integer, limit: 8
+    change_column :carts, :cart_sr_number, :integer, limit: 8, using: 'cart_sr_number::integer'
+    change_column :carts, :cart_number, :integer, limit: 8, using: 'cart_number::integer'
   end
 
 end
