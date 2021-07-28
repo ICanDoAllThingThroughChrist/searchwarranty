1caa5834863ccb5d10bd0b711523aa3d5189aba6
lee <ctlee9096@gmail.com>
2021-07-28 13:37:32 -0500

    troubleshoot missing end syntax in Sr model

--- db/migrate/20210716021957_add_att2to_srs.rb
+++
@@ -1,4 +0,0 @@
-class AddAtt2toSrs < ActiveRecord::Migration[5.2]
-  def change
-  end
-end
--- db/migrate/20210727194914_add_overdue_to_srs.rb
+++
@@ -1,5 +0,0 @@
-class AddOverdueToSrs < ActiveRecord::Migration[5.2]
-  def change
-    add_column :srs, :overdue, :string
-  end
-end
